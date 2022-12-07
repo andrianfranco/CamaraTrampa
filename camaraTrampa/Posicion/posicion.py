bfe2940c429119f1155d0b7d28a239843f0958cf
Franco Andrian <62945797+andrianfranco@users.noreply.github.com>
2022-12-07 19:22:36 -0300

    Add files via upload

---
+++ camaraTrampa/Posicion/posicion.py
@@ -0,0 +1,119 @@
+import time   							# Uso de delay 
+
+#DH/DAH	= Direccion horaria / antihoraria
+#EHA	= Eje hacia arriba
+
+#Cada paso recorre 1.8° -> 200 * 1.8° = 360°ca
+
+## Segun casos de prueba
+#  Posicion inicial a Posicion 1 = -90° + -45° = -135°/1.8° = 75 pasos DAH EHA
+#  Posicion inicial a Posicion 2 = 		  -45° = -45°/1.8° 	= 25 pasos DAH EHA
+#  Posicion inicial a Posicion 3 = 		   45° =  45°/1.8° 	= 25 pasos  DH EHA
+#  Posicion inicial a Posicion 4 =  90° +  45° =  135°/1.8° = 75 pasos  DH EHA
+
+nPasos		= 0               							# Número de pasos del motor. 
+microPausa 	= 0.005             						# Pausa por paso en segundos.
+finCarrera1 = True										# Estado de sensor de fin de carrera.
+
+#Funcion para posicionar/reposicionar el sistema en la posicion inicial
+
+def reposicionar (GPIO, pinFinCarrera1, pinDir, pinPaso):
+
+		finCarrera1 = GPIO.input(pinFinCarrera1)
+		if (finCarrera1 == True):						# Si finCarrera1 no esta presionado
+			GPIO.output(pinDir, 1)          			# Direccion 1, DAH EHA - giramos en sentido antihorario hasta ser presionado
+			
+			while (finCarrera1 == True):
+				GPIO.output(pinPaso, True)
+				time.sleep(microPausa)
+				GPIO.output(pinPaso, False)
+				time.sleep(microPausa)
+				finCarrera1 = GPIO.input(pinFinCarrera1)# Si se presiona el sensor, frena el motor
+										
+			GPIO.output(pinDir, 0)          			# Direccion 0, DH EHA - Cambiamos direccion de giro
+			nPasos = 100								# 100 pasos de 1.8° recorre 180° (Giramos hacia el extremo derecho).
+			for x in range(0,nPasos):
+				GPIO.output(pinPaso, True)
+				time.sleep(microPausa)
+				GPIO.output(pinPaso, False)
+				time.sleep(microPausa)
+
+# Funcion para posicionar el sistema en la posicion del sensor PIR activado 
+
+def posicionar (GPIO,posicionPIR, pinDir, pinPaso):
+
+	if (posicionPIR == 1): 								# pInit a P1 y a pInit
+		nPasos = 75
+		GPIO.output(pinDir, 1)          				# Direccion 1, DAH EHA
+		for x in range(0,nPasos):
+			GPIO.output(pinPaso, True)
+			time.sleep(microPausa)
+			GPIO.output(pinPaso, False)
+			time.sleep(microPausa)
+
+	elif (posicionPIR == 2):							# pInit a P2 y a pInit	
+		nPasos = 25
+		GPIO.output(pinDir, 1)          				# Direccion 1, DAH EHA
+		for x in range(0,nPasos):
+			GPIO.output(pinPaso, True)
+			time.sleep(microPausa)
+			GPIO.output(pinPaso, False)
+			time.sleep(microPausa)
+
+	elif (posicionPIR == 3): 							# pInit a P3 y a pInit
+		nPasos = 25
+		GPIO.output(pinDir,0)           				# Direccion 0, DH EHA
+		for x in range(0,nPasos):
+			GPIO.output(pinPaso, True)
+			time.sleep(microPausa)
+			GPIO.output(pinPaso, False)
+			time.sleep(microPausa)
+
+	elif (posicionPIR == 4):							# pInit a P4 y a pInit
+		nPasos = 75
+		GPIO.output(pinDir,0)           				# Direccion 0, DH EHA
+		for x in range(0,nPasos):
+			GPIO.output(pinPaso, True)
+			time.sleep(microPausa)
+			GPIO.output(pinPaso, False)
+			time.sleep(microPausa)
+
+# Funcion para retornar desde la posicion del sensor PIR activado a la posicion inicial. 
+
+def retornoPosInit(GPIO, posicionPIR, pinDir, pinPaso):
+
+	if (posicionPIR == 1): 								# pInit a P1 y a pInit
+		nPasos = 75                    	
+		GPIO.output(pinDir,0)           				# Direccion 0, DH EHA
+		for x in range(0,nPasos):
+			GPIO.output(pinPaso, True)
+			time.sleep(microPausa)
+			GPIO.output(pinPaso, False)
+			time.sleep(microPausa)
+
+	elif (posicionPIR == 2):							# pInit a P2 y a pInit	
+		nPasos = 25     	
+		GPIO.output(pinDir,0)           				# Direccion 0, DH EHA
+		for x in range(0,nPasos):
+			GPIO.output(pinPaso, True)
+			time.sleep(microPausa)
+			GPIO.output(pinPaso, False)
+			time.sleep(microPausa)
+
+	elif (posicionPIR == 3): 							# pInit a P3 y a pInit
+		nPasos = 25
+		GPIO.output(pinDir, 1)          				# Direccion 1, DAH EHA
+		for x in range(0,nPasos):
+			GPIO.output(pinPaso, True)
+			time.sleep(microPausa)
+			GPIO.output(pinPaso, False)
+			time.sleep(microPausa)
+
+	elif (posicionPIR == 4):							# pInit a P4 y a pInit
+		nPasos = 75
+		GPIO.output(pinDir, 1)          				# Direccion 1, DAH EHA
+		for x in range(0,nPasos):
+			GPIO.output(pinPaso, True)
+			time.sleep(microPausa)
+			GPIO.output(pinPaso, False)
+			time.sleep(microPausa) 
---
+++ camaraTrampa/Posicion/tiempo.py
@@ -0,0 +1,56 @@
+from string import octdigits
+import time 						# Importamos modulo de tiempo
+
+# Funcion booleana que determina si se debe reposicionar el sistema (transcurrio 1 hora desde el inicio o desde el ultimo reposicionamiento)
+# Retorno = 1 --> Si se requiere reposicionar
+# Retorno = 0 --> No se requiere reposicionar
+
+def transcurrio1Hora():
+	aux = False 					
+
+	## Toma de fecha y hora
+	anio 	= time.strftime("%y")	# Numero de año 2 digitos
+	mes 	= time.strftime("%m")	# Mes en numero
+	dia 	= time.strftime("%d")	# Dia del mes en numero
+	hora 	= time.strftime("%H")	# Formato 24H
+	minuto 	= time.strftime("%M")
+	segundo = time.strftime("%S")
+
+	DDMMAA = (int(dia)*10000)+(int(mes)*100)+(int(anio)*1)
+	HHMMSS = (int(hora)*10000)+(int(minuto)*100)+(int(segundo)*1)
+	
+	try: #Intentamos leer archivo con modo Read
+		f = open("reposicionamiento.txt", "r")
+		DDMMAAtxt = int(f.readline())
+		HHMMSStxt = int(f.readline())
+		f.close()
+		
+		# Si el dia es el mismo y la diferencia  es mayor o igual a 1 hora, reposiciona
+		# Si el dia es distinto y la diferencia entre la hora guardada y la hora final del dia, sumado a la  hora transcurrida en el nuevo dia es mayor a 1 hora, se reposiciona.
+		# Si transcurrieron mas de 1 dia, se reposiciona, sin importar diferencia horaria.
+		
+		if (((DDMMAA == DDMMAAtxt) and (HHMMSS -HHMMSStxt >= 10000)) or ((DDMMAA - DDMMAAtxt == 10000) and (235959 - HHMMSStxt + HHMMSS >= 10000)) or (DDMMAA - DDMMAAtxt > 10000)):
+				f = open("reposicionamiento.txt", "w")
+				f.write(dia+mes+anio + "\n" + hora+minuto+segundo)
+				f.close()
+				aux = True
+
+	except FileNotFoundError: #Si no existe archivo, se crear y  se carga la fecha y hora actual en formato DDMMAA /n HHMMSS
+		f = open("reposicionamiento.txt", "w")
+		f.write(dia+mes+anio + "\n" + hora+minuto+segundo)
+		f.close()
+
+		# En este caso no se reposiciona debido a que es la primer ocurrencia ya que el archivo no existe.
+
+	return aux
+
+def nombreArc(ubicacion):
+	##toma de fecha y hora
+	anio    = time.strftime("%y")  	# Numero de año 2 digitos
+	mes     = time.strftime("%m")  	# Mes en numero
+	dia     = time.strftime("%d")  	# Dia del mes en numero
+	hora    = time.strftime("%H") 	# Formato 24H
+	minuto  = time.strftime("%M")
+	segundo = time.strftime("%S")
+
+	return ubicacion+dia+'_'+mes+'_'+anio+'-'+hora+'_'+minuto+'_'+segundo  # Nombre del archivo
